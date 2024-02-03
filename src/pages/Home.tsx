805052df5954bfdb3cc8cf603ad5852456eca152
A-nirvana <anirban_ug_23@gmail.com>
2024-02-03 12:14:55 +0530

    Created the basic frame for the website

---
+++ src/pages/Home.tsx
@@ -0,0 +1,9 @@
+
+export default function Home(){
+    return (
+        <div style={{textAlign:"center", alignItems:"center"}}>
+            <h1>Welcome</h1>
+            <p>Everything you need as a Fresher</p>
+        </div>
+    )
+}

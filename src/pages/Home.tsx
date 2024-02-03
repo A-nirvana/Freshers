4400e83b41bd034d10ec2f1ff3d231f64f5bba0f
hirakRajbonshi <hrkraj0@gmail.com>
2024-02-03 22:41:49 +0530

    shifted to react project

--- src/pages/Home.tsx
+++
@@ -1,9 +0,0 @@
-
-export default function Home(){
-    return (
-        <div style={{textAlign:"center", alignItems:"center"}}>
-            <h1>Welcome</h1>
-            <p>Everything you need as a Fresher</p>
-        </div>
-    )
-}
